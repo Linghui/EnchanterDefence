db601ee2afd014d3f7598b0307b1e417e51ebd8c
李英辉 <xiaohuidexinge@163.com>
2015-03-10 00:23:39 +0800

    血条搞定

---
+++ EnchanterDefence/Assets/Scripts/BarController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarController : MonoBehaviour {
+	
+	public Sprite front;
+	
+	void Start(){
+		Sprite temp = Sprite.Create (front.texture, 
+		                             new Rect(0,0,front.texture.width/2,front.texture.height), 
+		                             new Vector2(1, 0.5f), 
+		                             front.pixelsPerUnit);
+		GetComponent<SpriteRenderer> ().sprite = temp;
+	}
+}
