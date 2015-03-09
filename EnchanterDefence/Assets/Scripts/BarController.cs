b22df81c08293027dc6bca2ddb61d40dbc6b13b4
李英辉 <xiaohuidexinge@163.com>
2015-03-10 01:18:11 +0800

    bar done.

--- EnchanterDefence/Assets/Scripts/BarController.cs
+++ EnchanterDefence/Assets/Scripts/BarController.cs
@@ -4,12 +4,33 @@ using System.Collections;
 public class BarController : MonoBehaviour {
 	
 	public Sprite front;
+
+	public GameObject owner;
 	
 	void Start(){
+
+		setupBarLength (1f);
+	}
+
+	void setupBarLength(float percent){
+		Debug.Log ("percent " + percent);
+		if(percent < 0 ){
+			percent = 0;
+		} else if ( percent > 1 ){
+			percent = 1;
+		}
+		int width = (int)(front.texture.width * percent);
+
+		Debug.Log ("width " + width);
+
+		
 		Sprite temp = Sprite.Create (front.texture, 
-		                             new Rect(0,0,front.texture.width/2,front.texture.height), 
-		                             new Vector2(1, 0.5f), 
+		                             new Rect(0,0,width,front.texture.height),
+		                             new Vector2(0.5f/percent, 0.5f), 
 		                             front.pixelsPerUnit);
+		temp.name = front.name;
+
 		GetComponent<SpriteRenderer> ().sprite = temp;
 	}
+
 }
