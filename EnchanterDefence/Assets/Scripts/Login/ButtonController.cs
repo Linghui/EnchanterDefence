aae38283e7d152c8a0be8ccef71643aa3117530e
李英辉 <xiaohuidexinge@163.com>
2015-03-22 16:33:11 +0800

    add login scene

---
+++ EnchanterDefence/Assets/Scripts/Login/ButtonController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+	void startGame(){
+		Debug.Log ("startGame");
+		Application.LoadLevel ("GameScene");
+	}
+
+}
